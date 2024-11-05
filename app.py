ad7134d8f295c16f2b73c447b3d4aa8bb2d23953
Aryan Bhajanka <85822773+aryanbhajanka@users.noreply.github.com>
2024-11-05 11:30:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,23 +17,19 @@ box = px.box(tips, y="total_bill", x="day", color='time')
 histogram = px.histogram(tips, x="tip", color="sex")
 
 st.title("Data Visualization with Plotly")
+st.markdown(""Created By: Aryan Bhajanka)
 
 st.header("Plot 1: Bar Plot - Tips by Day")
 st.plotly_chart(bar)
-st.markdown('''**Insight Observed**: (Add your insights here)''')
 
 st.header("Plot 2: Violin Plot - Tips by Gender")
 st.plotly_chart(violin)
-st.markdown('''**Insight Observed**: (Add your insights here)''')
 
 st.header("Plot 3: Scatter Plot - Total Bill vs. Tip (Color-coded by Gender)")
 st.plotly_chart(scatter)
-st.markdown('''**Insight Observed**: (Add your insights here)''')
 
 st.header("Plot 3: Box Plot -Distribution of Total Bill by Day (With Color by Time)")
 st.plotly_chart(box)
-st.markdown('''**Insight Observed**: (Add your insights here)''')
 
 st.header("Plot 4: Histogram Plot - Tip Distribution (With Color)")
 st.plotly_chart(histogram)
-st.markdown('''**Insight Observed**: (Add your insights here)''')
