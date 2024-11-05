6d0e4ac2c6d780aff33d9d8443961d18ee73ff63
Aryan Bhajanka <85822773+aryanbhajanka@users.noreply.github.com>
2024-11-05 10:52:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,26 @@ import streamlit as st
 tips = sns.load_dataset('tips')
 tips.head()
 
-fig1 = px.bar(tips, x="day", y="tip")
+bar = px.bar(tips, x="day", y="tip")
 
-fig2 = px.violin(tips, x="sex", y="tip")
+violin = px.violin(tips, x="sex", y="tip")
+
+scatter = px.scatter(tips, y="total_bill", x="tip")
+
+box = px.box(tips, y="total_bill", x="day")
+
+histogram = px.histogram(tips, x="tip", color="sex")
 
 st.title("Data Visualization with Plotly")
 
 st.header("Plot 1: Bar Plot - Tips by Day")
-st.plotly_chart(fig1)
+st.plotly_chart(bar)
 st.markdown('''**Insight Observed**: (Add your insights here)''')
 
 st.header("Plot 2: Violin Plot - Tips by Gender")
-st.plotly_chart(fig2)
+st.plotly_chart(violin)
+st.markdown('''**Insight Observed**: (Add your insights here)''')
+
+st.header("Plot 3: Scatter Plot - Tips by Gender")
+st.plotly_chart(scatter)
 st.markdown('''**Insight Observed**: (Add your insights here)''')
